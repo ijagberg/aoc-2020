bc1ff4162184763273f9cb43e347c8ed53c23ec6
Isak Jägberg <ijagberg@gmail.com>
2020-12-01 12:17:44 +0100

    Add some comments

--- examples/day1_part1.rs
+++ examples/day1_part1.rs
@@ -4,6 +4,10 @@ use aoc_2020::read_lines_from_file;
 
 fn main() {
     let input_file = std::env::args().nth(1).unwrap();
+    let target = std::env::args()
+        .nth(2)
+        .map(|s| s.parse().unwrap())
+        .unwrap_or(2020);
 
     let lines: Vec<String> = read_lines_from_file(&input_file)
         .unwrap()
@@ -12,15 +16,17 @@ fn main() {
 
     let nums = lines.into_iter().map(|l| l.parse().unwrap()).collect();
 
-    println!("{}", solve(nums).unwrap());
+    println!("{}", solve(nums, target).unwrap());
 }
 
-fn solve(nums: Vec<u32>) -> Result<u32, ()> {
-    const TARGET_SUM: u32 = 2020;
+/// Iterate over `nums`, finding the first pair of numbers that sum up to a given `target`
+/// # Returns
+/// The product of the pair of numbers, or () on failure (no pair in `nums` sum up to `target`)
+fn solve(nums: Vec<u32>, target: u32) -> Result<u32, ()> {
     let mut num_set = HashSet::new();
 
     for num in nums {
-        let target_diff = TARGET_SUM - num;
+        let target_diff = target - num;
         if num_set.contains(&target_diff) {
             return Ok(num * target_diff);
         } else {
--- examples/day1_part2.rs
+++ examples/day1_part2.rs
@@ -4,6 +4,10 @@ use aoc_2020::read_lines_from_file;
 
 fn main() {
     let input_file = std::env::args().nth(1).unwrap();
+    let target = std::env::args()
+        .nth(2)
+        .map(|s| s.parse().unwrap())
+        .unwrap_or(2020);
 
     let lines: Vec<String> = read_lines_from_file(&input_file)
         .unwrap()
@@ -12,13 +16,16 @@ fn main() {
 
     let nums = lines.into_iter().map(|l| l.parse().unwrap()).collect();
 
-    println!("{}", solve(nums).unwrap());
+    println!("{}", solve(nums, target).expect("unsolvable"));
 }
 
-fn solve(nums: Vec<u32>) -> Result<u32, ()> {
-    const TARGET_SUM: u32 = 2020;
+/// Iterate over `nums`, finding the first 3-tuple of numbers that sum up to a given `target`
+/// # Returns
+/// The product of the tuple of numbers, or () on failure (no 3-tuple in `nums` sum up to `target`)
+fn solve(nums: Vec<u32>, target: u32) -> Result<u32, ()> {
+    // create and populate a map of (i+j) -> (i,j)
+    // to allow for easier lookup
     let mut num_map = HashMap::new();
-
     for i in 0..nums.len() {
         let num_i = nums[i];
         for j in i + 1..nums.len() {
@@ -27,8 +34,10 @@ fn solve(nums: Vec<u32>) -> Result<u32, ()> {
         }
     }
 
+    // for each number, check if the target diff (target - num) has been found already in the map
+    // if it has been found, the product is num*i*j
     for num in nums {
-        let target_diff = TARGET_SUM - num;
+        let target_diff = target - num;
         if let Some(&(i, j)) = num_map.get(&target_diff) {
             return Ok(i * j * num);
         }
